8003088bde26e707b61cb4a6839a8440f7603498
Duvan Castro <duvancastro1026@gmail.com>
2023-04-09 16:15:09 -0500

    update file

--- src/main/java/com/example/Exercise1_Seccion456/controller/HelloController.java
+++ src/main/java/com/example/Exercise1_Seccion456/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.Exercise1_Seccion456;
+package com.example.Exercise1_Seccion456.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
