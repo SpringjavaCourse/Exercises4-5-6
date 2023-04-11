b7103b1445815251d18b83127d47e587aa7a7dfd
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 14:10:46 -0500

    file create

---
+++ src/test/java/com/example/Exercise1_Seccion456/controller/LaptopControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.Exercise1_Seccion456.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaptopControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findOneById() {
+    }
+
+    @Test
+    void create() {
+    }
+}
