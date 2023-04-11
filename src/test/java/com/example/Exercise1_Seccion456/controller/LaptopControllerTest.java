d5bfc795f96742bbe4949f4ea6811861e7cbedc4
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 14:47:40 -0500

    implement test for method findAll and findOneById

--- src/test/java/com/example/Exercise1_Seccion456/controller/LaptopControllerTest.java
+++ src/test/java/com/example/Exercise1_Seccion456/controller/LaptopControllerTest.java
@@ -1,17 +1,57 @@
 package com.example.Exercise1_Seccion456.controller;
 
+import com.example.Exercise1_Seccion456.entities.Laptop;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)// Asigna un puerto aleatorio para el test
 class LaptopControllerTest {
+
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder);
+    }
 
     @Test
     void findAll() {
+
+        ResponseEntity<Laptop[]> response =
+                testRestTemplate.getForEntity("/api/Laptops", Laptop[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(200, response.getStatusCodeValue());
+
     }
 
     @Test
     void findOneById() {
+
+        ResponseEntity<Laptop> response =
+                testRestTemplate.getForEntity("/api/Laptops/1", Laptop.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
     }
 
     @Test
