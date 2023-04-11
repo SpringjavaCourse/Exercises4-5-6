f3062e605e9a5f6755d0a98f1a22cabd25333300
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 11:49:58 -0500

    create and implement method findOneById

--- src/main/java/com/example/Exercise1_Seccion456/controller/LaptopController.java
+++ src/main/java/com/example/Exercise1_Seccion456/controller/LaptopController.java
@@ -2,28 +2,48 @@ package com.example.Exercise1_Seccion456.controller;
 
 import com.example.Exercise1_Seccion456.entities.Laptop;
 import com.example.Exercise1_Seccion456.repository.LaptopRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LaptopController {
 
     private LaptopRepository laptopRepository;
 
+    //Constructor
     public LaptopController(LaptopRepository laptopRepository) {
         this.laptopRepository = laptopRepository;
     }
 
-    // Busca todos los laptops
-    @GetMapping("/api/Laptops")
-    public List<Laptop> getListLaptop(){
 
-        return laptopRepository.findAll();
+    /**
+     * Busca todos los laptops
+     * @return List<Laptop> Recupera y devuelve todos los laptops de BD
+     */
+    @GetMapping("/api/Laptops")
+    public List<Laptop> findAll(){
+        return laptopRepository.findAll(); // Recupera y devuelve todos los laptops de BD
     }
+
+    /**
+     * Buscar Laptop por ID
+     * @param id id tipo Long a buscar
+     * @return ResponseEntity<Laptop> Respuesta Http
+     */
+    @GetMapping("/api/Laptops/{id}")
+    public ResponseEntity<Laptop> findOneById(@PathVariable Long id){
+
+        Optional<Laptop> LaptopOpt = laptopRepository.findById(id);
+
+        //Option 1
+        return LaptopOpt.isPresent() ? ResponseEntity.ok(LaptopOpt.get()) : ResponseEntity.notFound().build();
+        //Option 2
+        //return LaptopOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 
     // Creamos laptop
     @PostMapping("/api/Laptops")
