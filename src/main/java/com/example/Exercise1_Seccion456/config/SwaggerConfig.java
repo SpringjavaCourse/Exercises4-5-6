ba6aca1626674083540056ab0f1ef2c2fe215d21
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 13:51:10 -0500

    implementation Swagger

--- src/main/java/com/example/Exercise1_Seccion456/config/SwaggerConfig.java
+++ src/main/java/com/example/Exercise1_Seccion456/config/SwaggerConfig.java
@@ -1,4 +1,47 @@
 package com.example.Exercise1_Seccion456.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * Configuración Swagger para la generación de la documentación de la API REST
+ * HTML: http://localhost:8081/swagger-ui/
+ * JSON: http://localhost:8081/v2/api-docs
+ */
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiDetails(){
+
+        return new ApiInfo("Spring Boot Laptop API REST",
+                "Library API REST docs",
+                "1.0",
+                "http://www.google.com",
+                new Contact("Duvan","http://www.google.com","duvancastro1026@gmail.com"),
+                "MIT",
+                "http://www.google.com",
+                Collections.emptyList());
+    }
+
 }
+
--- src/main/java/com/example/Exercise1_Seccion456/repository/LaptopRepository.java
+++ src/main/java/com/example/Exercise1_Seccion456/repository/LaptopRepository.java
@@ -1,7 +1,6 @@
 package com.example.Exercise1_Seccion456.repository;
 
 import com.example.Exercise1_Seccion456.entities.Laptop;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LaptopRepository extends JpaRepository<Laptop, Long> {
