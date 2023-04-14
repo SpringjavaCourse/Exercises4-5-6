dfda2a2d1b730ea8ce2322d851552291b9cd17fc
duvan1026 <duvancastro1026@gmail.com>
2023-04-14 14:58:21 -0500

    implement solution exercise 1 and 2 of the section 11,12 and 13

--- src/main/java/com/example/Exercise1_Seccion456/controller/HelloController.java
+++ src/main/java/com/example/Exercise1_Seccion456/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.Exercise1_Seccion456.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping("/api/saludo")
     public String saludo(){
+        System.out.println(this.message);
         return "Hola mundo, como estan?...";
     }
 }
