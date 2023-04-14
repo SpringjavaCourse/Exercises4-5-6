dedd035e3fafdaf5be56701e33cfb0ed149328a2
duvan1026 <duvancastro1026@gmail.com>
2023-04-14 15:30:24 -0500

    implement application security

---
+++ src/main/java/com/example/Exercise1_Seccion456/security/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exercise1_Seccion456.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
